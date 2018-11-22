83cfc8081c3ec62cc75dfec42c2a5c251c926f37
jusloh <jusloh@outlook.com>
2018-11-22 17:42:43 +0800

    Displaying Live Data from OpenWeatherMap

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  appId: 'a15ff74856dbe21502ce405bb6cf5bdf',
+  baseUrl: 'http://',
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
